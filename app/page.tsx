42a6ee16f368e5728d8c5469f3073ac5d96c6da7
spencerdowie <spencer.dowie@hotmail.com>
2026-08-21 16:44:49 -0400

    [+] Basic layout of site [+] Added mui components [+] Remote image path to a1 api

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,140 @@
+"use client";
+import { Masonry } from "@mui/lab";
 import Image from "next/image";
+import { PropsWithChildren, useEffect, useState } from "react";
+
+const apiURL = "https://fullstack-8ksk.onrender.com/";
+
+type Project = {
+  name: string;
+  title: string;
+  releaseYear: string;
+  thumbnailPath?: string;
+  description: string;
+  languages: string[];
+};
+
+interface BubbleProps extends PropsWithChildren {
+  selected?: boolean;
+  onClick?: () => void;
+}
+
+function Bubble({ children, selected, onClick }: BubbleProps) {
+  const colour = selected ? "bg-blue-900 border-2" : "bg-blue-950";
+  return (
+    <div
+      className={"py-1 px-3 rounded-4xl shrink hover:bg-blue-800 " + colour}
+      onClick={onClick}>
+      {children}
+    </div>
+  );
+}
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  const [projects, SetProjects] = useState<Project[]>([]);
+  const [languages, SetLanguages] = useState<string[]>([]);
+  const [filter, SetFilter] = useState<string>("");
+
+  function GetProjects() {
+    return projects
+      .filter((project) => filter == "" || project.languages.includes(filter))
+      .map((project) => (
+        <div key={project.name} className="bg-gray-900 rounded-md p-4 flex flex-col">
+          <div className="mb-3">
+            <h3 className="font-bold text-xl mb-2">{project.name}</h3>
+            <h4 className="font-semibold text-lg ps-4">{project.title}</h4>
+          </div>
+          {project.thumbnailPath != undefined ? (
             <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
+              src={apiURL + "images/" + project.thumbnailPath}
+              alt={"Thumbnail for " + project.name}
+              width={300}
+              height={150}
+              className="self-center"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          ) : null}
+          <p>{project.description}</p>
+          <div className="flex flex-row flex-wrap gap-2 mt-3">
+            {project.languages.map((lang) => (
+              <Bubble key={lang}>{lang}</Bubble>
+            ))}
+          </div>
+        </div>
+      ));
+  }
+
+  async function FetchPortfolioData(): Promise<Project[]> {
+    const langs: string[] = [];
+    const projs: Project[] = [];
+    await fetch(apiURL + "api/web")
+      .then((res) => res.json())
+      .then((projRes: Project[]) => {
+        projRes.forEach((project) => projs.push(project));
+      });
+
+    await fetch(apiURL + "api/games")
+      .then((res) => res.json())
+      .then((projRes: Project[]) => {
+        projRes.forEach((project) => (project.languages = ["Unity", "C#"]));
+        projRes.forEach((project) => projs.push(project));
+      });
+
+    return projs;
+  }
+
+  useEffect(() => {
+    const langs: string[] = [];
+    FetchPortfolioData().then((projs) => {
+      projs.forEach((project) =>
+        project.languages.forEach((lang) => {
+          if (!langs.includes(lang)) langs.push(lang);
+        })
+      );
+      SetLanguages(langs);
+      SetProjects(projs);
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans bg-mist-950">
+      <main className="flex flex-1 w-full max-w-7xl flex-col items-center justify-between py-16 px-16  sm:items-start">
+        <div className="flex flex-row items-baseline gap-5 mb-16">
+          <h1 className="text-4xl/7 font-bold text-white">Spencer Dowie</h1>
+          <h2 className="text-2xl/7 font-bold text-white">
+            Full-Stack Web Developer
+          </h2>
+        </div>
+        <div className="flex flex-row">
+          <div className="flex flex-col flex-1">
+            <h2 className="text-2xl font-bold">About Me</h2>
+            <p>
+              A web developer with a background in game programming. I have 2+
+              years of experience in web development and over 8 years of
+              experience in game programming as part of an indie game studio
+              that I co-founded.
+            </p>
+          </div>
+          <div className="flex flex-col flex-2">
+            <h2 className="text-2xl font-bold mb-3">Projects</h2>
+            <div className="flex flex-row mx-2 mb-3 gap-2">
+              {languages.map((language) => {
+                const isFilter = filter === language;
+                return (
+                  <Bubble
+                    key={language}
+                    selected={isFilter}
+                    onClick={() =>
+                      isFilter ? SetFilter("") : SetFilter(language)
+                    }>
+                    {language}
+                  </Bubble>
+                );
+              })}
+            </div>
+            <Masonry columns={2} spacing={3}>
+              {GetProjects()}
+            </Masonry>
+          </div>
         </div>
       </main>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://fullstack-8ksk.onrender.com/**")]
+  }
 };
 
 export default nextConfig;
