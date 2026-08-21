7583bdd7b7db7eb841f7d292ce1d626058d24e2d
spencerdowie <spencer.dowie@hotmail.com>
2026-08-21 18:10:17 -0400

    [+] Added social links to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,27 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <header className="ps-4 py-3 bg-gray-900 flex flex-row items-center">
-          <h1 className="text-4xl/7 font-bold text-white absolute">Spencer Dowie</h1>
+          <h1 className="text-4xl/7 font-bold text-white absolute">
+            Spencer Dowie
+          </h1>
           <div className="flex flex-row flex-1 justify-center items-center gap-5">
-            <div>LinkedIn BSky GitHub</div>
+            <div className="flex gap-6">
+              <a
+                className="text-lg font-semibold"
+                href="https://www.linkedin.com/in/spencer-dowie/">
+                LinkedIn
+              </a>
+              <a
+                className="text-lg font-semibold"
+                href="https://github.com/spencerdowie">
+                GitHub
+              </a>
+              <a
+                className="text-lg font-semibold"
+                href="mailto:spencer.dowie@hotmail.com">
+                Email
+              </a>
+            </div>
           </div>
         </header>
         {children}
