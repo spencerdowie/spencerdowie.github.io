5c34bdcd1bf690f8c4dcc6fb434e60dfef9379aa
spencerdowie <spencer.dowie@hotmail.com>
2026-09-01 17:29:36 -0400

    [-] Removed remote image url from next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL("https://fullstack-8ksk.onrender.com/**")]
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
